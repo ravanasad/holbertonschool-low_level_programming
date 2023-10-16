aaf906eb2c00d1961c93583dd8f169c4e7fa815d
Ravan Asadov <7397@holbertonstudents.com>
2023-10-16 02:03:57 -0700

    Updated 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@
  * Return: Always 0
  */
 
-int main(void)
+void print_alphabet(void)
 {
 	int n = 97;
 	for ( ; n <= 122; n++)
@@ -13,5 +13,4 @@ int main(void)
 		_putchar(n);
 	}
 	_putchar('\n');
-	return (0);
 }
