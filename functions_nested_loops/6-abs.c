c1d766eb7a20e53827b6c7cd48bfe5404eb081e8
Ravan Asadov <7397@holbertonstudents.com>
2023-10-16 02:42:36 -0700

    Created 6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - checks for lowercase character
+ * @c: first param
+ * Return: Always return abs c
+ */
+
+int _abs(int c)
+{
+	return (abs(c));
+}
