eb659249e1d8ed6daa229900a76b31a38ea19b2d
Ravan Asadov <7397@holbertonstudents.com>
2023-10-16 02:01:43 -0700

    Created 1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet, in lowercase
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 97;
+	for ( ; n <= 122; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
