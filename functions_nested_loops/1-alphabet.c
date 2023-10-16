c464fdebefc7cf3cbd7317652a55cc0674f8eb15
Ravan Asadov <7397@holbertonstudents.com>
2023-10-16 02:05:39 -0700

    Updated 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * main - prints the alphabet, in lowercase
+ * print_alphabet - prints the alphabet, in lowercase
  * Return: Always 0
  */
 
 void print_alphabet(void)
 {
 	int n = 97;
-	for ( ; n <= 122; n++)
+
+	for (; n <= 122; n++)
 	{
 		_putchar(n);
 	}
