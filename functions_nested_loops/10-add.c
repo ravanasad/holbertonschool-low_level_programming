5e1ac8740cfcac107d7b9c6b077b2666bfd84206
Ravan Asadov <7397@holbertonstudents.com>
2023-10-17 12:03:18 -0700

    Created 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and
+ * returns the result
+ * @a: first number
+ * @b: second number
+ * Return: sum of two numbers
+ */
+
+int add(int a, int b)
+{
+	return a+b;
+}
