3502b1f9f2ec0804fc578da096cd8adb4aa9fd58
Ravan Asadov <7397@holbertonstudents.com>
2023-10-18 02:11:09 -0700

    Created 5-more_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,6 +2,7 @@
 #include <ctype.h>
 /**
  * print_most_numbers - prints the numbers from 0 to 9
+ * except 2 and 4
  */
 void print_most_numbers(void)
 {
---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * more_numbers - prints 10 times the numbers,
+ * from 0 to 14
+ */
+void more_numbers(void)
+{
+	int i, j = 0;
+
+	for (; j < 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
