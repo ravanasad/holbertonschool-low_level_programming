027f94d13e01016f4ec5dd9853ec89078c706a15
Ravan Asadov <7397@holbertonstudents.com>
2023-10-18 02:01:11 -0700

    Created 4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * print_most_numbers - prints the numbers from 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
