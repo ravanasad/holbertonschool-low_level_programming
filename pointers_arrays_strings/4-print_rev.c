344e87ba2cf890e29b0e15c14442e55cbae0a075
Ravan Asad <ravanasad@outlook.com>
2023-10-25 01:16:46 -0700

    Created 4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints a string, in reverse
+ * @str: first param.
+ */
+void print_rev(char *str)
+{
+	int i = strlen(str);
+
+	for (; i >= 0; i--)
+	{
+		_putchar(str[i]);
+	}
+}
