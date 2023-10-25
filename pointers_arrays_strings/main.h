4c94320ff07eae6897cdb09feb02596ee62e874c
Ravan Asad <ravanasad@outlook.com>
2023-10-25 02:46:34 -0700

    Created 5-rev_string.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,4 +3,5 @@ void print_to_98(int n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-void print_rev(char *s);
+void print_rev(char *str);
+void rev_string(char *s);
