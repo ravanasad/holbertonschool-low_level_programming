e3bf89165ae69e9ea6c2b8756ca7e2e36570e1c7
Ravan Asad <ravanasad@outlook.com>
2023-10-26 00:36:38 -0700

    Updated 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,26 +1,31 @@
 #include <stdbool.h>
 #include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @str: number for checking
+ * Return: number in str
+ */
 
 int _atoi(char *str)
 {
-    int i = 0, val = 0;
-    bool isNegative = false;
-    
-    for (; str[i] != '\0'; i++) 
-    {
-        if (str[i] == '-')
-        {
-            isNegative = !isNegative;
-        }
-        else if (str[i] >= '0' && str[i] <= '9')
-        {
-            val = (val * 10) + (str[i] - '0');
-        }
-        else if (val > 0)
-        {
-            break;
-        }
-    }
-    val = isNegative ? val * -1 : val;
-    return (val);
+	int i = 0, val = 0;
+	bool isNegative = false;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if (str[i] == '-')
+		{
+			isNegative = !isNegative;
+		}
+		else if (str[i] >= '0' && str[i] <= '9')
+		{
+			val = (val * 10) + (str[i] - '0');
+		}
+		else if (val > 0)
+		{
+			break;
+		}
+	}
+	val = isNegative ? val * -1 : val;
+	return (val);
 }
