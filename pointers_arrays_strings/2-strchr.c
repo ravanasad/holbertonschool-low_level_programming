a31a856913b927c7e815621b9bb64e53966ea75b
Ravan Asad <ravanasad@outlook.com>
2023-10-29 01:41:51 -0700

    Created 2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: the string
+ * @c: Characters that we find in a string
+ * Return: pointer to the first occurrence of the
+ * character c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i]; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (NULL);
+}
