bd9940c501511014794c4d7e256c5a9f2efb8357
Ravan Asad <ravanasad@outlook.com>
2023-10-29 02:41:29 -0700

    Updated again

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -9,9 +9,9 @@ char *_strstr(char *haystack, char *needle)
 {
 	int index = 0, i = 0;
 	
-	if (needle == 0)
+	if (*needle == 0)
 	{
-		return haystack;
+		return (haystack);
 	}
 
 	while (haystack[index])
