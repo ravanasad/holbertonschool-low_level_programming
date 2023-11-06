01ce9068de1960e72ddc7bddc1bd41c91524dba7
Ravan Asad <ravanasad@outlook.com>
2023-11-06 10:44:18 -0800

    Updated 0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,10 +11,13 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *arr = malloc(size * sizeof(char));
+	char *arr;
 	unsigned int i = 0;
 
 	if (size == 0)
+		return (NULL);
+	*arr = malloc(size * sizeof(char));
+	if (arr == 0)
 		return (NULL);
 	for (; i < size; i++)
 	{
