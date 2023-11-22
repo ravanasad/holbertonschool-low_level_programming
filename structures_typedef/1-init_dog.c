2e69fe5cd635abaa6d5dad48c34b74deba77738d
Ravan Asad <ravanasad@outlook.com>
2023-11-21 22:40:06 -0800

    Created 1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->owner = owner;
+	d->age = age;
+}
