8ab0046d50d21ab38cbb37bf2232f7ab68f2e924
Ravan Asad <ravanasad@outlook.com>
2023-11-25 14:32:19 -0800

    Updated again

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -55,9 +55,9 @@ int op_div(int a, int b)
 int op_mod(int a, int b)
 {
 	if (b == 0)
-        {
-                printf("Error\n");
-                exit(100);
-        }
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
