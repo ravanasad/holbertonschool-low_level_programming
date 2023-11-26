57049c3a4cf48b1242803f7477383e77fcf35a1b
Ravan Asad <ravanasad@outlook.com>
2023-11-26 06:58:18 -0800

    Created 2-print_strings.c

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - prints numbers
+ * @separator: separator
+ * @n: params count
+ * @...: params
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i = 0;
+
+	va_start(list, n);
+
+	for (; i < n; i++)
+	{
+		printf("%s", va_arg(list, char *));
+		if (i != (n - 1) && separator)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
