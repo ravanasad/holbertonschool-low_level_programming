1b1d112e6cef0f8c998d8df9f343ca90eeb783af
Ravan Asad <ravanasad@outlook.com>
2023-12-05 14:04:52 -0800

    Updated

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,13 +12,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *node;
 
 	node = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (node == NULL)
+		return (NULL);
+
 	node->n = n;
 	if (node == NULL)
 		return (NULL);
 	if ((*head) == NULL)
 	{
 		node->next = NULL;
-		node->prev = NULL;	
+		node->prev = NULL;
 	}
 	else
 	{
