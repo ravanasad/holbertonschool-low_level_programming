c4be1beef0318b46640435a92acbb891ac9ea274
Ravan Asad <ravanasad@outlook.com>
2023-12-10 04:44:21 -0800

    Updated 1-print_binary.c

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -2,15 +2,35 @@
 #include <stdio.h>
 /**
  * print_binary - prints the binary representation of a number
- * @n: number
- */
+ * @n: number to print in binary
+ * Return: void
+*/
 void print_binary(unsigned long int n)
 {
-	char a;
+unsigned long int i;
+int check = 0;
 
-	if (n == '\0')
-		return;
-	print_binary(n >>= 1);
-	a =  (n & 1) ? '1' : '0';
-	_putchar(a);
+if (n == 0)
+{
+_putchar('0');
+return;
+}
+
+for (int i = 31; i >= 0; i--)
+{
+if (check)
+{
+if (n>>i & 1)
+{
+_putchar('1');
+}
+else
+_putchar('0');
+}
+else if (n>>i & 1)
+{
+_putchar('1');
+check = 1;
+}
+}
 }
