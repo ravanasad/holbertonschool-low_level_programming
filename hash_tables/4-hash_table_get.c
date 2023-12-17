fe9389334bbc79204e72229edd681dbf35921f68
Ravan Asad <ravanasad@outlook.com>
2023-12-17 09:23:04 -0800

    Updated  4-hash_table_get.c

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -9,12 +9,12 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned int idx = key_index((const unsigned char *)key, ht->size);
+	unsigned int idx;
 	hash_node_t *node;
 
-	if (!ht)
+	if (ht == NULL)
 		return (NULL);
-
+	idx = key_index((const unsigned char *)key, ht->size);
 	if (idx > ht->size)
 		return (NULL);
 	node = ht->array[idx];
